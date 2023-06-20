b37749fd0611b2a64dd768f04d09fb0419db2dbc
dannythedev <paramountclairvoyance@gmail.com>
2023-06-20 03:06:15 +0300

    Added the faster LevCinema API.

--- Cinemas/LevCinema.py
+++ Cinemas/LevCinema.py
@@ -1,4 +1,7 @@
 import datetime
+import json
+from collections import defaultdict
+
 from Cinemas.Cinema import Cinema
 from Functions import is_english, regexify, find_nearest_addresses, IMAGE_NOT_FOUND, suffixify, filter_hour_format, \
     sort_and_remove_duplicate_hours
@@ -12,7 +15,7 @@ class LevCinema(Cinema):
         super().__init__()
         self.home_url = 'https://www.lev.co.il/'
         self.url = '{home_url}en/movies/'.format(home_url=self.home_url)
-        self.api_url = '{home_url}api/presentations?includeSynopsis=0'
+        self.api_url = 'https://ticket.lev.co.il/api/presentations?includeSynopsis=0'
         self.name = 'Lev Cinema'
 
     def get_movies(self):
@@ -47,28 +50,27 @@ class LevCinema(Cinema):
         Then filters out branches that are outside a 20 km radius.
         Returns list of dictionaries as such:
         [{id:'', latitude:'', longitude:'', displayName:'', url:''}]"""
-        return []
+        response = self.get(self.api_url)
+        self.html.set(response)
+        response = json.loads(response.text)['presentations']
+        self.html.response = response
+        theatres = list(set([data['locationName'] for data in response]))
+        return theatres
 
     def get_theatre_screenings(self, theatres, movies=None):
         """Gets all movie screenings from theatres list.
         Returns dictionary which values are a list of dictionaries as such:
         {theatre1:{movieid1: screenings, movieid2: screenings...}, theatre2:{movieid3: screenings}}"""
 
-        timetables = dict()
-        today = datetime.datetime.now().date().strftime('%d/%m')
-        for movie in movies:
-            self.get(movie.url)
-            theatres_in_movie = self.html.get_xpath("//div[@class='forloc 2']")
-            theatres_in_movie = [etree.tostring(x, pretty_print=True) for x in theatres_in_movie]
-            for theatre in theatres_in_movie:
-                self.html.set(theatre)
-                theatre_display_name = str(self.html.get_xpath("//div[@class='showlist 456'][1]//a[@class='mobilelink']/@data-loc")[0])
-                if theatre_display_name not in timetables:
-                    timetables.update({theatre_display_name:dict()})
-                # Check if next screening is today.
-                if regexify(r'\d{2}/\d{2}', self.html.get_xpath("//div[@class='showlist 456']/span[1]/text()")[0]) == today:
-                    screenings = filter_hour_format(self.html.get_xpath("//div[@class='showlist 456'][1]//a[@class='mobilelink']/text()"))
-                    if screenings:
-                        timetables[theatre_display_name].update({movie.origin[self.name]:sort_and_remove_duplicate_hours(screenings)})
+        timetables = {key: dict() for key in theatres}
+        today = datetime.datetime.now().date().strftime('%Y-%m-%d')
+        for theatre in theatres:
+            screenings = [(str(data['featureId']), regexify(r'\d{2}:\d{2}', data['dateTime'])) for data in self.html.response
+                          if data['locationName'] == theatre and
+                          regexify(r'\d{4}-\d{2}-\d{2}',data['dateTime']) == today]
+            movie_dict = defaultdict(list)
+            for movie_id, time in screenings:
+                movie_dict[movie_id].append(time)
+                timetables[theatre] = dict(movie_dict)
             self.progress += 1
         return timetables
