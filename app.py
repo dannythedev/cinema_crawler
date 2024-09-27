be77c5387688e10bd82eb802300fc55106e35215
dannythedev <paramountclairvoyance@gmail.com>
2024-09-27 17:42:18 +0300

    Added fetch function.

--- app.py
+++ app.py
@@ -1,19 +1,57 @@
 from flask import Flask, jsonify
 import json
+import threading
+import time
+from Archive import Archive
+
+class GetData:
+    def __init__(self):
+        sources = {
+            'YesPlanet': 1,
+            'HotCinema': 1,
+            'CinemaCity': 1,
+            'LevCinema': 1,
+            'Metacritic': 1,
+            'RottenTomatoes': 1,
+            'IMDB': 1,
+            'TheMovieDB': 1,
+            'Letterboxd': 0
+        }
+        self.archive = Archive(
+            checklist=[name for name, active in sources.items() if active],
+            is_screenings=False
+        )
+
+    def start(self):
+        self.archive.initialize_cinemas()
+        self.archive.initialize_reviewers()
+        self.archive.export_json()
+        print(".json exported.")
+
+    def run_periodically(self, interval):
+        while True:
+            self.start()
+            print("Waiting for {} seconds before the next run...".format(interval))
+            time.sleep(interval)
 
 app = Flask(__name__)
 
-# Load movies from the movies.json file
 def load_movies():
     with open('movies.json') as f:
         movies = json.load(f)
     return movies
 
-# Route to serve the JSON data
 @app.route('/movies', methods=['GET'])
 def get_movies():
     movies = load_movies()
     return jsonify(movies)
+
+@app.route('/fetch', methods=['GET'])
+def fetch():
+    # Start fetching data in a new thread
+    data_collector = GetData()
+    data_collector.start()
+    return jsonify({'success': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
